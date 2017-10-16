170e97e8f6aff85349e73b2b8305a8446dbba947
Anarthal (Ruben Perez) <rubenperez038@gmail.com>
2017-10-16 19:55:23 +0200

    Added NumberField test

---
+++ tests/contact-forms/test-number-field.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+_abaco_require('inc/contact-forms/field.php');
+
+class NumberFieldTest extends WP_UnitTestCase {
+ 
+    /**
+     * @dataProvider validation_cases
+     * Set $expected to null to indicate we expect validation to fail
+     */
+    function test_validate($input, $expected) {
+        $field = new ABACO_NumberField('name', 'display', true);
+        $res = $field->validate($input);
+        if (is_null($expected)) {
+            $this->assertInstanceof(Exception::class, $res);
+        } else {
+            $this->assertEquals($expected, $res);
+        }
+    }
+    
+    function validation_cases() {
+        return [
+            [[], null],
+            ['5908invalid', null],
+            ['0', 0],
+            ['89', 89]
+        ];
+    }
+
+}
