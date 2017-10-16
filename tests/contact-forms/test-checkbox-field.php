ab272f8561c3bf1cc621ec4bd8eb2d525b17c761
Anarthal (Ruben Perez) <rubenperez038@gmail.com>
2017-10-16 21:38:38 +0200

    Added CheckboxField and MulticheckboxField tests

--- tests/contact-forms/test-checkbox-field.php
+++ tests/contact-forms/test-checkbox-field.php
@@ -34,4 +34,24 @@ class CheckboxFieldTest extends WP_UnitTestCase {
             [['value', ''], true]
         ];
     }
+    
+   /**
+     * @dataProvider code_cases
+     */
+    function test_code($display, $opts, $expected) {
+        $field = new ABACO_CheckboxField('name', $display, $opts);
+        $this->assertEquals($expected, $field->code());
+    }
+    
+    function code_cases() {
+        return [
+            ['display', [], '<p>[checkbox name "display"]</p>'],
+            ['display', ['cf7_options' => 'opts'],
+                '<p>[checkbox name opts "display"]</p>'],
+            ['display', ['element_id' => 'test'],
+                '<p id="test">[checkbox name "display"]</p>'],
+            ['display"inject', [], '<p>[checkbox name "display&quot;inject"]</p>'],
+            ['display<p>[]', [], '<p>[checkbox name "display&lt;p&gt;"]</p>']
+        ];
+    }
 }
--- tests/contact-forms/test-multicheckbox-field.php
+++ tests/contact-forms/test-multicheckbox-field.php
@@ -35,4 +35,31 @@ class MulticheckboxFieldTest extends WP_UnitTestCase {
             [['invalid'], null, true]
         ];
     }
+    
+    /**
+     * @dataProvider code_cases
+     */
+    function test_code($display, $mandatory, $opts, $expected) {
+        $field = new ABACO_MulticheckboxField('name', $display, $mandatory,
+            ['opt1', 'opt2'], $opts);
+        $this->assertEquals($expected, $field->code());
+    }
+    
+    function code_cases() {
+        return [
+            ['display', false, [], '<p>display<br />[checkbox name]</p>'],
+            ['display', true, ['asterisk' => false],
+                '<p>display<br />[checkbox* name]</p>'],
+            ['display', false, ['asterisk' => true],
+                '<p>display ' . ABACO_FieldParams::label_asterisk_html() . 
+                '<br />[checkbox name]</p>'],
+            ['display', false, ['cf7_options' => 'opts'],
+                '<p>display<br />[checkbox name opts]</p>'],
+            ['display', false, ['element_id' => 'test'],
+                '<p id="test">display<br />[checkbox name]</p>'],
+            ['<script>display', false, [],
+                '<p>&lt;script&gt;display<br />[checkbox name]</p>'],
+            ['[display]', false, [], '<p>display<br />[checkbox name]</p>'],
+        ];
+    }
 }
