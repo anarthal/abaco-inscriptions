bc614a341aebd82656b980d75372e4d51e838528
Anarthal (Ruben Perez) <rubenperez038@gmail.com>
2018-02-22 18:56:00 +0100

    Removed filter that may cause interactions with other custom post types (the one to show activities)

--- abaco.php
+++ abaco.php
@@ -163,8 +163,8 @@ add_action('wp_head', function() {
     ABACO_NirvanaActivity::register_hooks();
 });
 
-add_action('pre_get_posts', function($query) {
-    if ($query->is_main_query() && $query->is_front_page()) {
+/*add_action('pre_get_posts', function($query) {
+    if (!is_admin() && $query->is_main_query()) {
         $query->set('post_type', array('post', ABACO_ACTIVITY_POST_TYPE_NAME));
     }
-});
+});*/
